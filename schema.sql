9f2bfaf8c433a113a01f9fce414dba050cd42f01
Akasha <Akasha.Rojee@gmail.com>
2021-09-29 16:03:49 +0400

    Add index for visits.vets_id and owners.email

--- schema.sql
+++ schema.sql
@@ -48,5 +48,12 @@ insert into owners (full_name, email) select 'Owner ' || generate_series(1,25000
 CREATE INDEX IX_animals_visits_id ON visits (animals_id);
 DROP INDEX IX_animals_visits_id;
 
+-- Optimize visits table by creating an Index using the vets_id column
+CREATE INDEX IX_vets_id ON visits (vets_id);
+DROP INDEX IX_vets_id;
+
+-- -- Optimize owners table by creating an Index using the email column
+CREATE INDEX IX_email_id ON owners (email ASC);
+DROP INDEX IX_email_id;
 
 
